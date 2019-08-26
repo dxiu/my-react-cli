8073267f03cb6f7ffa1e91405111da6a003d3b3d
dingxiu <914467193@qq.com>
2019-08-26 14:33:34 +0800

    上传图片成blob

--- src/views/user.js
+++ src/views/user.js
@@ -1,23 +1,56 @@
 import React, { Component } from 'react'
 // import axios from 'axios'
-import { upload } from '../api/personData'
+// import { upload } from '../api/personData'
 export default class User extends Component{
-  
+  constructor() {
+    super()
+    this.state = {
+      name: 'dx',
+      file: [],
+      url: ''
+    }
+    this.files = []
+  }
   uploadImg(e) {
-    const files = Array.from(e.target.files)
+    this.files = Array.from(e.target.files)
+    this.setState({
+      file: this.files
+    })
+    this.studyBlob()
     const formData = new FormData();  // 创建form对象
-    files.forEach((item) => {
+    this.files.forEach((item) => {
       formData.append('file', item)
     })
-    upload(formData).then(res => {
-      console.log(res)
+    // upload(formData).then(res => {
+    //   console.log(res)
+    // })
+  }
+  studyBlob() {
+    const blob = new Blob(this.files)
+    // createObjectURL
+    const url = URL.createObjectURL(blob)
+    // FileReader
+    // const fileReder = new FileReader()
+    // fileReder.readAsDataURL(blob)
+    // fileReder.onload = (res) => {
+    //   this.setState({
+    //     url: res.target.result
+    //   })
+    // }
+    this.setState({
+      url: url
     })
+  }
+  componentDidUpdate() {
+    
   }
   render(){
     return(
       <div>
-        <input accept="image/*" type="file" multiple onChange={this.uploadImg}/>
-        用户中心
+        <input accept="image/*" type="file" multiple onChange={this.uploadImg.bind(this)}/>
+        {this.state.am ?(this.state.am+this.state.name):this.state.name}
+        <button onClick={this.studyBlob.bind(this)}>blob</button>
+        <img src={this.state.url} alt="" />
       </div>
     )
   }
