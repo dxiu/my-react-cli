b0cedd36f1e0b3a5c3642f133d4b2f4fb199ef6c
dingxiu <914467193@qq.com>
2019-08-29 17:51:50 +0800

    静态

---
+++ build/precache-manifest.b22891c594f07060dd54271266994978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff622010cfe5ea7e4a2c66e77884f2dd",
+    "url": "/dxiu/my-react-cli/index.html"
+  },
+  {
+    "revision": "4f8165e6afc054d75e6b",
+    "url": "/dxiu/my-react-cli/static/css/2.61b8705f.chunk.css"
+  },
+  {
+    "revision": "78f44763b7c753a2b764",
+    "url": "/dxiu/my-react-cli/static/css/main.835f3d3c.chunk.css"
+  },
+  {
+    "revision": "4f8165e6afc054d75e6b",
+    "url": "/dxiu/my-react-cli/static/js/2.dc54527e.chunk.js"
+  },
+  {
+    "revision": "78f44763b7c753a2b764",
+    "url": "/dxiu/my-react-cli/static/js/main.c6a5619e.chunk.js"
+  },
+  {
+    "revision": "fc723e0d8b82aba913d2",
+    "url": "/dxiu/my-react-cli/static/js/runtime~main.b2bd8bc5.js"
+  }
+]);
